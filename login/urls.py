5733f8be50a75bfb5331e4a769e6841a9f4956cf
396235146 <palinfeng@163.com>
2018-10-29 09:58:43 +0800

    11

---
+++ login/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+app_name='login'
+urlpatterns = [
+    url(r'^index/', views.index,name='logindex'),
+    url(r'^login/', views.login,name='login'),
+    url(r'^register/', views.register,name='logregister'),
+    url(r'^logout/', views.logout,name='logout'),
+    ]
